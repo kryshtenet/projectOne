7c682af7ccb15ec87a24aa33e7471075564e8681
Kryshtene Tesfaye <kryshtenet@outlook.com>
2020-02-08 11:46:31 -0500

    Setting up Project

---
+++ assets/scripts/main.js
@@ -0,0 +1,24 @@
+/*Liam's code goes here*/
+
+/*Liam's code ends here*/
+
+
+
+/*Oscar's code goes here*/
+
+/*Oscar's code ends here*/
+
+
+
+/*Kryshtene's code goes here*/
+
+/*Kryshtene's code ends here*/
+
+
+
+/*Gianni's code goes here*/
+
+/*Gianni's code ends here*/
+
+
+/*If you add your own java script, make a comment in the script of the index.html.*/
